bb5f6c43c8e399f262d052eb490eab94ed983a1c
nyoh17 <yoh17@jh.edu>
2023-10-24 16:19:58 -0400

    merged

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -8,34 +8,37 @@ from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
 
+from ask_sdk_model.ui import SimpleCard
 from ask_sdk_model import Response
 
 import openai
 
 sb = SkillBuilder()
-openai.api_key = # Add your OpenAI API key as a default value here
+openai.api_key = '' # Add your OpenAI API key as a default value here
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 SYSTEM_INSTRUCTIONS = (
-        "You are a warm receptionist whose primary purpose is to help college students make appointments with doctors. "
+        "You are a warm AI receptionist whose primary purpose is to help college students make appointments with doctors. "
         "You help users identify medical specialists and book appointments based on their symptoms. "
         "Follow these steps:"
         "1. Ask for the user's symptoms. Get as much information as you can by asking follow-up questions if the symptoms are vague or unclear. "
-        "2. Suggest 2 to 3 types of relevant medical specialists based on the symptoms, and ask which one the user would like to see. "
+        "2. Suggest 2 to 3 types of relevant medical specialists based on the symptoms, and explain what each medical specialist specializes in. Ask which one the user would like to see. "
         "3. Ask for the user's location to help you find medical specialists in their area. "
-        "4. Suggest (or make up) 1 to 2 medical specialists in their area. Ask the user for their preference. "
-        "The user may ask for reviews on the practitioner, in which case just make up a rating out of 5 stars. "
+        "4. Suggest 2 medical specialists (or make them up). For each medical specialist, make up a rating out of 5 stars and a short sentence customer review. Ask the user for their preference. "
         "5. Ask for the user's medical insurance provider. "
-        "6. Reply that their insurance is accepted by the specialist, and let the user choose from 2 to 3 time slots to meet with the doctor. "
+        "6. Reply that their insurance is accepted by the specialist, and let the user choose from 2 to 3 time slots within the next 3 days (within 8 AM to 6 PM) to meet with the doctor. "
         "7. Confirm the appointment details with the user. "
         "8. If the user confirms appointment details, state that the user should receive a confirmation email and a reminder before their appointment. "
         "Otherwise, go back to an earlier step. "
         "9. Ask if there's anything else you can help with, then end the conversation. "
         "Remember, your role is to help the user while avoiding unnecessary repetition during the conversation. "
         "Avoid repeating statements like 'I am an AI language model ...' or 'You should consult medical professionals...' if you have already mentioned it in the current conversation. "
-        "Keep your response brief."
+        "Avoid compliments like 'Great choice...'. "
+        "Keep each response direct to the point and under 65 words. "
+        "Keep each response professional. "
     )
 
 @sb.request_handler(can_handle_func=is_intent_name("MessageIntent"))
@@ -66,19 +69,15 @@ def report_symptom_request_handler(handler_input: HandlerInput) -> Response:
     handler_input.response_builder.speak(speech_text).set_should_end_session(False)
     return handler_input.response_builder.response
 
-
 @sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
 def launch_request_handler(handler_input):
     """Handler for Skill Launch."""
     # type: (HandlerInput) -> Response
-    speech_text = "How can I help? Please describe the purpose of your appointment. "
+    speech_text = "Welcome to Doctor Finder. How can I help? Please describe your symptoms or needs. "
     
     # Instantiate session messages
     session_attr = handler_input.attributes_manager.session_attributes
-    session_attr['messages'] = [
-        {"role": "system", "content": SYSTEM_INSTRUCTIONS},
-        {"role": "assistant", "content": speech_text},
-    ]
+    session_attr['messages'] = [{"role": "system", "content": SYSTEM_INSTRUCTIONS}]
 
     return handler_input.response_builder.speak(speech_text).set_should_end_session(False).response
 
@@ -87,7 +86,7 @@ def launch_request_handler(handler_input):
 def help_intent_handler(handler_input):
     """Handler for Help Intent."""
     # type: (HandlerInput) -> Response
-    speech_text = "You can ask me for help booking an appointment with a doctor."
+    speech_text = "You can ask me for finding a medical specailist and booking an appointment with them."
 
     return handler_input.response_builder.speak(speech_text).ask(speech_text).response
 
@@ -99,7 +98,7 @@ def help_intent_handler(handler_input):
 def cancel_and_stop_intent_handler(handler_input):
     """Single handler for Cancel and Stop Intent."""
     # type: (HandlerInput) -> Response
-    speech_text = "Goodbye!"
+    speech_text = "Farewell. Please open Doctor Finder again if you need to find a doctor."
 
     return handler_input.response_builder.speak(speech_text).response
 
@@ -112,7 +111,7 @@ def fallback_handler(handler_input):
     """
     # type: (HandlerInput) -> Response
     speech = (
-        "The Doctor Appointment Scheduler skill can't help you with that.  "
+        "The Doctor Finder skill can't help you with that.  "
         "You can ask for help booking an appointment.")
     reprompt = "You can ask for help booking an appointment."
     handler_input.response_builder.speak(speech).ask(reprompt)
